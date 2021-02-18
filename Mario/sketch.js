93a7bc28336f87da0b3ba4adcc551cec57d1be20
Aaryadeva <73352730+Aaryadeva@users.noreply.github.com>
2021-02-18 16:18:59 +0530

    Add files via upload

--- Mario/sketch.js
+++ Mario/sketch.js
@@ -1,18 +1,24 @@
-var mario,mario_standingImg,mario_runningImg,ground,groundImg,bg,bgImg,coin,coinImg
-var enemy,enemyImg,mushroom,mushroomImg,obstacle,obstacleImg,enemyKill,brickImg
+var mario,mario_standingImg,mario_runningImg,mario_collidedImg,ground,groundImg,bg,bgImg,coin,coinImg
+var enemy,enemyImg,mushroom,mushroomImg,obstacle,obstacleImg,enemyKill,brickImg,restart,restartImg,castleImg,castle
 var totalCoins = 0
+var m=120;
+var distance=0;
+var enemyGroup,enemyKillGroup
+var gameState="play"
 var enemyGroup,enemyKillGroup
 function preload(){
   mario_standingImg=loadAnimation("mario00.png")
   mario_runningImg=loadAnimation("mario00.png","mario01.png","mario02.png","mario03.png")
+  mario_collidedImg=loadAnimation("collided.png")
+ 
   groundImg=loadImage("ground2.png")
   bg=loadImage("bg.png")
   obstacleImg=loadAnimation("obstacle1.png","obstacle2.png","obstacle3.png","obstacle4.png")
   enemyImg=loadImage("enemy.png")
   brickImg=loadImage("brick.png")
   coinImg=loadImage("coin.png")
-
-
+  restartImg=loadImage("restart.png")
+  castleImg= loadImage("castle.png")
 }
 
 function setup(){
@@ -20,14 +26,20 @@ function setup(){
   mario=createSprite(10,height-100,20,20)
   mario.addAnimation("standing",mario_standingImg)
   mario.addAnimation("running",mario_runningImg)
-  ground=createSprite(0,height-90,width*4,20)
+  mario.addAnimation("collided",mario_collidedImg)
+  mario.setCollider("rectangle",0,3,20,25)
+  
+  ground=createSprite(0,height-80,width*4,20)
   ground.visible=false
+
+  restart=createSprite(mario.x+100,height-200)
+  restart.addImage(restartImg)
+  restart.visible=false
+
+  castle=createSprite(mario.x+250,height-100)
+  castle.addImage(castleImg)
+  castle.visible=false
   
-   // coin.visible=false
-  
-  
- // brick1=createSprite()
- 
   
   enemyGroup=createGroup()
   enemyKillGroup=createGroup()
@@ -38,63 +50,120 @@ function setup(){
   
 }
 
-function draw(){
-  background(bg)
+function draw()
+{
+    background(bg)
   
-  camera.position.x=mario.x+350
-  ground.x=mario.x
-  
-  //mario.debug=true
-  //enemy.debug=true
-  if(keyIsDown(RIGHT_ARROW)){
-    mario.changeAnimation("running",mario_runningImg)
-    mario.x=mario.x+20
-  }else{
-    mario.changeAnimation("standing",mario_standingImg)
-  }
-  if(keyDown(UP_ARROW)&&mario.y>=509){
-    mario.velocityY=mario.velocityY-10
-  }
-  text(mario.x,200,200)
+   if(gameState==="play")
+    {
+      if (frameCount % round(frameRate()) === 0 && m > -1) {
+    
+        m --;
+       
+      }
+      camera.position.x=mario.x+350
+      ground.x=mario.x
+      mario.collide(ground)
+      console.log(mario.y)
+      if(keyIsDown(RIGHT_ARROW))
+      {
+        mario.changeAnimation("running",mario_runningImg)
+        mario.x=mario.x+20
+        distance++;
+      }
+      else
+      {
+        mario.changeAnimation("standing",mario_standingImg)
+      }
 
-  if(keyIsDown(LEFT_ARROW)){
-    mario.changeAnimation("running",mario_runningImg)
-    mario.x=mario.x-5
-  }else{
-    mario.changeAnimation("standing",mario_standingImg)
+      if(keyIsDown(LEFT_ARROW))
+      {
+        mario.changeAnimation("running",mario_runningImg)
+        mario.x=mario.x-5
+        distance--;
+      }
+      else
+      {
+        mario.changeAnimation("standing",mario_standingImg)
+      }
+
+      if(keyDown("space")&&mario.y>=462)
+      {
+        mario.velocityY=mario.velocityY-10
+      }
+      
+      mario.velocityY=mario.velocityY+0.5
+
+      var r =Math.round(random(1,2))
+      if(frameCount%100===0)
+      {
+        if(r===1)
+        spawnBricks()
+        else
+        spawnObstacles()
+      }
+      spawnEnemies()
+      if(enemyKillGroup.isTouching(mario))
+      {
+        enemyGroup.destroyEach()
+      }
+      
+      if(brickGroup.isTouching(mario))
+      {
+        spawnCoins();
+        brickGroup.destroyEach();
+        coin.velocityY=-5
+        coin.lifetime=10
+        totalCoins=totalCoins+1
+      }
+      if(distance>=100)
+      {
+        gameState="end"
+        castle.visible=true
+        fill("black")
+       textSize(30)
+       text("YOU WON",mario.x-100,height/2);
+      }
+      
+      if(enemyGroup.isTouching(mario)||obstacleGroup.isTouching(mario)||m===0)
+      {
+        mario.changeAnimation("collided",mario_collidedImg);
+       gameState="end";
+       fill("black")
+       textSize(30)
+       text("GAME OVER",mario.x-100,height/2);
+       restart,visible=true
+
+      }
   }
-  if(enemyKillGroup.isTouching(mario)){
     
-    enemyGroup.destroyEach()
-  }
-  if(enemyGroup.isTouching(mario)||obstacleGroup.isTouching(mario)){
-    mario.visible=false
-  }
-  if(brickGroup.isTouching(mario)){
-    //brick1.destroy()
-   // coin.visible=true
-   coin=createSprite(mario.x,mario.y-20,10,10)
-    coin.addImage(coinImg)
-    coin.scale=0.05
-    coin.velocityY=-5
-    coin.lifetime=50
-    totalCoins=totalCoins+1
-  }
-  
-  mario.velocityY=mario.velocityY+0.5
-  mario.collide(ground)
-  //mario.collide(brick1)
-  mario.setCollider("rectangle",0,3,20,25)
-  /*if(mario.isTouching(coinGroup)){
+  if(gameState==="end")
+  {
     
-    coinGroup.destroyEach()
-  }*/
+    mario.velocityY=0
+    distance=0
+    brickGroup.destroyEach();
+    enemyGroup.destroyEach();
+    obstacleGroup.destroyEach();
+      }
+  if(mousePressedOver(restart))
+  {
+    reset();
+  }
   drawSprites()
-  text("Coins: "+totalCoins,camera.position.x-180,10)
-  //console.log(mouseY)
-  spawnEnemies()
-  spawnBricks()
+  text("Coins: "+totalCoins,mario.x-180,50)
+  text("Time: "+m,mario.x-300,50)
+  
 }
+
+function reset()
+{
+  gameState="play";
+  mario.changeAnimation("standing",mario_standingImg);
+  totalCoins=0
+  m=120
+}
+
 function spawnEnemies(){
   if(frameCount%400===0){
   enemy=createSprite(width,height-96,20,20)
@@ -117,11 +186,8 @@ function spawnBricks(){
     brick1=createSprite(width-200,height-180,20,20)
     brick1.addImage(brickImg)
     brick1.scale=0.8
-    brick1.velocityX=-3
-    
     brickGroup.add(brick1)
-    //coinGroup.add(coin)
-  }
+    }
 }
 function spawnObstacles(){
   if(frameCount%700===0){
@@ -131,3 +197,9 @@ function spawnObstacles(){
     obstacleGroup.add(obstacle)
   }
 }
+function spawnCoins()
+  {
+    coin=createSprite(mario.x,mario.y-20,10,10)
+    coin.addImage(coinImg)
+    coin.scale=0.05
+  }
